db15c30d149a73f4d01b47dbc3d02a9540db06f6
alpah121 <jacob_a_reed@yahoo.com>
2020-04-04 01:02:16 -0400

    updated bet functions

--- casino.sol
+++ casino.sol
@@ -4,7 +4,12 @@ contract  Casino {
     uint bank =0;
     address owner;
     
-    
+    createPossibleBet("1st12", 1, 12, 3);
+    createPossibleBet("2nd12", 13, 24, 3);
+    createPossibleBet("3rd12", 25, 36, 3);
+    createPossibleBet("19-36", 19, 36, 2);
+    createPossibleBet("1-18", 1, 18, 2);
+
     constructor() public
     {
     owner = msg.sender;
@@ -22,20 +27,32 @@ contract  Casino {
     
     mapping (string => possibleBet) possibleBets;
     
-    function setInstructor(address _address, uint _age, string _fName, string _lName) public {
-        var instructor = instructors[_address];
+    function createPossibleBet(string _text, uint _start, uint _end, uint _multiplier) internal {
+        var currentBet = possibleBets[_text];
 
-        instructor.age = _age;
-        instructor.fName = _fName;
-        instructor.lName = _lName;
-        
-        instructorAccts.push(_address) -1;
-
+        currentBet.start = _start;
+        currentBet.end = _end;
+        currentBet.multiplier = _multiplier;
     }
     
-    function didWin(section, try, winner) internal
+    function didWin(string section,uint try,uint winner) internal
     {
-    
+    //check for two length numbers
+    if (section == "numbers" && try.length == 2 && try == winner)
+        {
+        return true;
+        }
+    //otherwise check for odd or even and black or red
+    else if ( (section == "red" || section == "odd") && winner % 3 == 0)
+        {
+        return true;
+        }
+    else if ( (section == "black" || section == "even") && winner % 2 == 0)
+        {
+        return true;
+        }
+    //if all else fails check the possible bet array
+    else if ()
     }
    
     function multiplier() internal
